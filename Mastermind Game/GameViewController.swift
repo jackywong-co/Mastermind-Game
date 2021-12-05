4cc2eb168fe6bb00fd16d0f7f09657281c4c9abc
Jacky Wong <chikonwong@gmail.com>
2021-12-06 00:30:26 +0800

    Initial Commit

---
+++ Mastermind Game/GameViewController.swift
@@ -0,0 +1,63 @@
+//
+//  GameViewController.swift
+//  Mastermind Game
+//
+//  Created by Jacky Wong on 4/12/2021.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var num1Label: UILabel!
+    @IBOutlet weak var x1y1Box: UIButton!
+    @IBOutlet weak var x1y2Box: UIButton!
+    @IBOutlet weak var x1y3Box: UIButton!
+    @IBOutlet weak var x1y4Box: UIButton!
+    
+    @IBOutlet weak var result1Box: UIButton!
+    @IBOutlet weak var result2Box: UIButton!
+    @IBOutlet weak var result3Box: UIButton!
+    @IBOutlet weak var result4Box: UIButton!
+    
+    @IBOutlet weak var check1Btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        x1y1Box.layer.cornerRadius = x1y1Box.frame.width / 2
+//        x1y1Box.layer.masksToBounds = true
+//
+//        x1y2Box.layer.cornerRadius = x1y2Box.frame.width / 2
+//        x1y2Box.layer.masksToBounds = true
+//
+//        x1y3Box.layer.cornerRadius = x1y3Box.frame.width / 2
+//        x1y3Box.layer.masksToBounds = true
+//
+//        x1y4Box.layer.cornerRadius = x1y4Box.frame.width / 2
+//        x1y4Box.layer.masksToBounds = true
+//
+//        result1Box.layer.cornerRadius = result1Box.frame.width / 2
+//        result1Box.layer.masksToBounds = true
+//        result2Box.layer.cornerRadius = result2Box.frame.width / 2
+//        result2Box.layer.masksToBounds = true
+//        result3Box.layer.cornerRadius = result3Box.frame.width / 2
+//        result3Box.layer.masksToBounds = true
+//        result4Box.layer.cornerRadius = result4Box.frame.width / 2
+//        result4Box.layer.masksToBounds = true
+//
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mastermind Game/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Mastermind Game
+//
+//  Created by Jacky Wong on 4/12/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
