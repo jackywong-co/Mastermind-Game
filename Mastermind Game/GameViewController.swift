3dcba12a1cfcf0448e241da859be1d9d1de2cee9
Jacky Wong <chikonwong@gmail.com>
2021-12-14 14:26:01 +0800

    completed set cell size

--- Mastermind Game/GameViewController.swift
+++ Mastermind Game/GameViewController.swift
@@ -7,26 +7,65 @@
 
 import UIKit
 
+
 class GameViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     var game : Game = Game()
+
+
     
     // MARK: - Outlets
     @IBOutlet var codeButtons: [UIButton]!
     @IBOutlet var colorButtons: [UIButton]!
+    @IBOutlet var inputButtons: [UIButton]!
     @IBOutlet var checkButton: UIButton!
     @IBOutlet weak var colorView: UIStackView!
-    
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setGame()
-        tableView.transform = CGAffineTransform(scaleX: 1, y: -1)
-        game.record.append([game.addRound(round: 1), game.addImput(input1: 2, input2: 0, input3: 1, input4: 2)])
-        game.record.append([game.addRound(round: 2), game.addImput(input1: 1, input2: 1, input3: 2, input4: 2)])
+        // set table
+        self.tableView.transform = CGAffineTransform(scaleX: 1, y: -1)
+        self.tableView.rowHeight = 55
+        
+
+
+        
         print(game.record)
+        let tapGestureRecognizer0 = UITapGestureRecognizer(target: self, action: #selector(handleTapOnImageView(gestureRecognizer:)))
+        let tapGestureRecognizer1 = UITapGestureRecognizer(target: self, action: #selector(handleTapOnImageView(gestureRecognizer:)))
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target: self, action: #selector(handleTapOnImageView(gestureRecognizer:)))
+        let tapGestureRecognizer3 = UITapGestureRecognizer(target: self, action: #selector(handleTapOnImageView(gestureRecognizer:)))
+        let tapGestureRecognizer4 = UITapGestureRecognizer(target: self, action: #selector(handleTapOnImageView(gestureRecognizer:)))
+        let tapGestureRecognizer5 = UITapGestureRecognizer(target: self, action: #selector(handleTapOnImageView(gestureRecognizer:)))
        
+        colorButtons[0].addGestureRecognizer(tapGestureRecognizer0)
+        colorButtons[1].addGestureRecognizer(tapGestureRecognizer1)
+        colorButtons[2].addGestureRecognizer(tapGestureRecognizer2)
+        colorButtons[3].addGestureRecognizer(tapGestureRecognizer3)
+        colorButtons[4].addGestureRecognizer(tapGestureRecognizer4)
+        colorButtons[5].addGestureRecognizer(tapGestureRecognizer5)
     }
+    
+    @IBAction func resetInput1(_ sender: UIButton) {
+        sender.setImage(getColor(code: 8), for: .normal)
+        game.inputPegs[0] = 8
+    }
+    @IBAction func resetInput2(_ sender: UIButton) {
+        sender.setImage(getColor(code: 8), for: .normal)
+        game.inputPegs[1] = 8
+    }
+    @IBAction func resetInput3(_ sender: UIButton) {
+        sender.setImage(getColor(code: 8), for: .normal)
+        game.inputPegs[2] = 8
+    }
+    @IBAction func resetInput4(_ sender: UIButton) {
+        sender.setImage(getColor(code: 8), for: .normal)
+        game.inputPegs[3] = 8
+    }
+    
+    
     // MARK: - Get start the game
     func setGame(){
         self.game = Game()
@@ -43,23 +82,25 @@ class GameViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
-    func getColor(code : Int) -> UIImage {
-        switch(code){
-        case 0:
-            return UIImage(named: "Red")!
-        case 1:
-            return UIImage(named: "Blue")!
-        case 2:
-            return UIImage(named: "Yellow")!
-        case 3:
-            return UIImage(named: "Green")!
-        case 4:
-            return UIImage(named: "Purple")!
-        case 5:
-            return UIImage(named: "Brown")!
-        default:
-            return UIImage(named: "Black")!
+    @objc func handleTapOnImageView(gestureRecognizer: UITapGestureRecognizer) {
+        print("tag : \(gestureRecognizer.view?.tag ?? 0)")
+       
+        if game.inputPegs[0] == 8 {
+            game.inputPegs[0] = gestureRecognizer.view?.tag ?? 8
+            inputButtons[0].setImage(getColor(code: Int(gestureRecognizer.view?.tag ?? 8)), for: .normal)
+        } else if game.inputPegs[1] == 8 {
+            game.inputPegs[1] = gestureRecognizer.view?.tag ?? 8
+            inputButtons[1].setImage(getColor(code: Int(gestureRecognizer.view?.tag ?? 8)), for: .normal)
+        } else if game.inputPegs[2] == 8 {
+            game.inputPegs[2] = gestureRecognizer.view?.tag ?? 8
+            inputButtons[2].setImage(getColor(code: Int(gestureRecognizer.view?.tag ?? 8)), for: .normal)
+        } else if game.inputPegs[3] == 8 {
+            game.inputPegs[3] = gestureRecognizer.view?.tag ?? 8
+            inputButtons[3].setImage(getColor(code: Int(gestureRecognizer.view?.tag ?? 8)), for: .normal)
         }
+
+              
+
     }
     
     // MARK: - Table
@@ -81,18 +122,67 @@ class GameViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         // set pin
         cell.pinButtons[0].setImage(getColor(code: game.record[indexPath.row][2][0]), for: .normal)
-        cell.pinButtons[0].setImage(getColor(code: game.record[indexPath.row][2][1]), for: .normal)
-        cell.pinButtons[0].setImage(getColor(code: game.record[indexPath.row][2][2]), for: .normal)
-        cell.pinButtons[0].setImage(getColor(code: game.record[indexPath.row][2][3]), for: .normal)
+        cell.pinButtons[1].setImage(getColor(code: game.record[indexPath.row][2][1]), for: .normal)
+        cell.pinButtons[2].setImage(getColor(code: game.record[indexPath.row][2][2]), for: .normal)
+        cell.pinButtons[3].setImage(getColor(code: game.record[indexPath.row][2][3]), for: .normal)
 
         cell.transform = CGAffineTransform(scaleX: 1, y: -1)
         return cell
     }
     
+    @IBAction func check(_ sender: Any) {
+        print(game.code)
+        
+        // append record
+        game.record.append([game.addRound(round: 1), game.addImputs(input1: game.inputPegs[0], input2: game.inputPegs[1], input3: game.inputPegs[2], input4: game.inputPegs[3]),game.addPins()])
+        
+        print(game.record)
+        
+        // reset all input button
+        for inputButton in inputButtons {
+            
+            inputButton.setImage(getColor(code: 8), for: .normal)
+        }
+        // reset input pegs
+        game.inputPegs = [8,8,8,8]
+        
+        print(game.round)
+        
+        tableView.reloadData()
+        
+//        tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+
+    }
+
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+    }
+    
+    func getColor(code : Int) -> UIImage {
+        switch(code){
+        case 0:
+            return UIImage(named: "Red")!
+        case 1:
+            return UIImage(named: "Blue")!
+        case 2:
+            return UIImage(named: "Yellow")!
+        case 3:
+            return UIImage(named: "Green")!
+        case 4:
+            return UIImage(named: "Purple")!
+        case 5:
+            return UIImage(named: "Brown")!
+        case 6:
+            return UIImage(named: "Black")!
+        case 7:
+            return UIImage(named: "White")!
+        case 8:
+            return UIImage(named: "Question Mark")!
+        default:
+            return UIImage(named: "Question Mark")!
+        }
     }
 }
 
--- Mastermind Game/Model/Game.swift
+++ Mastermind Game/Model/Game.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 class Game {
-  
+    
     var code : [Int]
     var record : [[[Int]]]
     var inputPegs : [Int]
     var pins : [Int]
     
     var round = 0
-   
+    
     
     init(){
         self.code = []
@@ -23,50 +23,88 @@ class Game {
             let ansPes = Int.random(in: 0...5)
             code.append(ansPes)
         }
+    
         self.record = []
-        self.inputPegs = []
+        self.inputPegs = [8,8,8,8]
         self.pins = []
     }
-    func addImput(input1: Int,input2: Int,input3: Int,input4: Int) -> [Int]{
+    func addRound(round:Int) -> [Int]{
+        self.round += 1
+        return [round]
+    }
+    
+    
+ 
+    func addImputs(input1: Int,input2: Int,input3: Int,input4: Int) -> [Int]{
+        self.inputPegs = []
         self.inputPegs.append(input1)
         self.inputPegs.append(input2)
         self.inputPegs.append(input3)
         self.inputPegs.append(input4)
         return [input1,input2,input3,input4]
     }
-    func addRound(round:Int) -> [Int]{
-        return [round]
+  
+    func addPins() -> [Int]{
+        print(inputPegs)
+        var resultPins = [Int]()
+        var checked = [0,0,0,0]
+        // check color and position
+        for i in 0..<4{
+            if inputPegs[i] == code[i]{
+                resultPins.append(6)
+                checked[i] = 1
+            }
+            
+        }
+        // check color only
+        for i in 0..<4{
+            for j in 0..<4{
+                if i != j {
+                    if inputPegs[i] == code[j]{
+                        if checked[i] == 0 {
+                            resultPins.append(7)
+                        checked[i] = 1
+                        }
+                    }
+                }
+            }
+        }
+        
+        for _ in 0..<4{
+            if resultPins.count < 4 {
+                resultPins.append(8)
+            }
+        }
+        
+        for i in checked{
+            print(checked[i])
+        }
+        return resultPins
     }
     
-//    func check() -> [Int] {
-//        let pegsByRow = inputPegs[round]
-//        var checked = [0, 0, 0, 0]
-//        var pinsByRow = [Int]()
-//        //check color and pos
-//        for i in 0..<4 {
-//            if inputPegs[i] == pegsByRow[i] {
-//                pinsByRow.append(1)
-//                checked[i] = 1
-//            }
-//        }
-//        //check color only
-//        for i in 0..<4 {
-//            for j in 0..<4 {
-//                if i != j {
-//                    if inputPegs[i] == pegsByRow[j] {
-//                        if checked[i] == 0 {
-//                            pinsByRow.append(0)
-//                            checked[i] = 1
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        self.pins[round] = pinsByRow
-//        return pinsByRow
-//    }
-//
-    func nextRound(){
-        round += 1
+    
+    func setInputPegs() -> [Int]{
+        inputPegs = [8,8,8,8]
+        return inputPegs
     }
+    
+    func generateRandomNumber(_ from:Int, _ to:Int, _ qut:Int?) -> [Int] {
+        var randomNumbers = [Int]()
+        var numberOfNumbers = qut
+        
+        let lower = UInt32(from)
+        let highter = UInt32(to-1)
+        
+        if numberOfNumbers == nil || numberOfNumbers! > (to-from) + 1{
+            numberOfNumbers = (to-from) + 1
+        }
+        while randomNumbers.count != numberOfNumbers {
+            let num = arc4random_uniform(highter - lower) + lower
+            if !randomNumbers.contains(Int(num)){
+                randomNumbers.append(Int(num))
+            }
+        }
+        return randomNumbers
+    }
+    
 }
